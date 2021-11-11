9e0bbb1e2437c30fd1291d0023dda11af865e0ba
renaissancenerd <vatsallimbachiya@gmail.com>
2021-11-11 13:16:10 +0100

    updating url

--- ontology.js
+++ ontology.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const size = 20;
-const baseUrl = "http://service.tib.eu/ts4tib/api/ontologies/filterby?schema=collection&classification=NFDI4CHEM";
+const baseUrl = "https://service.tib.eu/ts4tib/api/ontologies/filterby?schema=collection&classification=NFDI4CHEM";
 const baseUrl2 = "http://service.tib.eu/ts4tib/api/ontologies"
 const settings = { method: "Get", headers: {'Accept': 'application/json'}};
 
