acf9c419d2e3e11cec60a4e5d0a2f51a9015775b
Oliver Koepler <oliver@koepler.de>
2022-02-12 22:13:44 +0100

    added cors() for express

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const ontology = require("./app_modules/ontology");
 const chemOntology = require("./app_modules/chemOntology");
 const ingOntology = require("./app_modules/ingOntology");
 const termsModule = require("./app_modules/term");
 const propertiesModule = require("./app_modules/property");
 var bodyParser = require("body-parser");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 const port = 8000;
+
+var corsOptions = {
+  origin: "http://localhost",
+};
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -22,48 +29,40 @@ app.get("/", async function (req, res) {
   res.send(data);
 });
 
-app.get("/ontologies", async function (req, res) {
+app.get("/ontologies", cors(), async function (req, res) {
   let data = await ontology.getOntologies();
-  res.set("Access-Control-Allow-Origin", "*");
   res.send(data);
 });
-app.get("/ontologies/chemistry", async function (req, res) {
+app.get("/ontologies/chemistry", cors(), async function (req, res) {
   let data = await chemOntology.getChemOntologies();
-  res.set("Access-Control-Allow-Origin", "*");
   res.send(data);
 });
-app.get("/ontologies/engineering", async function (req, res) {
+app.get("/ontologies/engineering", cors(), async function (req, res) {
   let data = await ingOntology.getIngOntologies();
-  res.set("Access-Control-Allow-Origin", "*");
   res.send(data);
 });
 
-app.get("/ontologies/:ontologyId", async function (req, res) {
+app.get("/ontologies/:ontologyId", cors(), async function (req, res) {
   let data = await ontology.getOneOntology(req.params.ontologyId);
-  res.set("Access-Control-Allow-Origin", "*");
   res.send(data);
 });
 
-app.get("/rootterms/:ontologyId", async function (req, res) {
+app.get("/rootterms/:ontologyId", cors(), async function (req, res) {
   let data = await termsModule.getRootTerms(req.params.ontologyId);
-  res.set("Access-Control-Allow-Origin", "*");
   res.send(data);
 });
 
-app.post("/termchildren", async function (req, res) {
+app.post("/termchildren", cors(), async function (req, res) {
   let data = await termsModule.getChildren(req.body.childrenLink);
-  res.set("Access-Control-Allow-Origin", "*");
   res.send(data);
 });
 
-app.get("/rootproperties/:ontologyId", async function (req, res) {
+app.get("/rootproperties/:ontologyId", cors(), async function (req, res) {
   let data = await propertiesModule.getRootProperties(req.params.ontologyId);
-  res.set("Access-Control-Allow-Origin", "*");
   res.send(data);
 });
 
-app.post("/propertychildren", async function (req, res) {
+app.post("/propertychildren", cors(), async function (req, res) {
   let data = await propertiesModule.getChildren(req.body.childrenLink);
-  res.set("Access-Control-Allow-Origin", "*");
   res.send(data);
 });
